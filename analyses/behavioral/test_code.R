fed90756427e1fc58dedc55ac21f0afdbc929ebc
n-mesuter@kingcounty.gov <n-mesuter@ph.lcl>
2021-12-07 13:02:53 -0800

    Initial behavioral health code

---
+++ analyses/behavioral/test_code.R
@@ -0,0 +1,9 @@
+db_hhsaw <- DBI::dbConnect(odbc::odbc(),
+                           driver = "ODBC Driver 17 for SQL Server",
+                           server = "tcp:kcitazrhpasqlprp16.azds.kingcounty.gov,1433",
+                           database = "hhs_analytics_workspace",
+                           uid = keyring::key_list("hhsaw")[["username"]],
+                           pwd = keyring::key_get("hhsaw", keyring::key_list("hhsaw")[["username"]]),
+                           Encrypt = "yes",
+                           TrustServerCertificate = "yes",
+                           Authentication = "ActiveDirectoryPassword")

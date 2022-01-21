1f0a8fc1929ce64ff42b810f7bc6d1bcd481c299
Megan Suter <n-mesuter@kingcounty.gov>
2022-01-21 12:53:33 -0800

    Setting up analysis, starting left joins

---
+++ analyses/behavioral/Initial_BH_Code.R
@@ -0,0 +1,10 @@
+##01/21/2022
+#Initial Code--Behavioral Analysis
+
+# #Steps
+# 1) Connect to HHSAW SQL server
+# 2) Read in appropriate data tables
+# 3) Use cross-walk tables to join tables of interest
+#   a) Study ID to KCID
+#   b) KCID to auth_id file (for crisis events)
+# 4) Demographic comparisons

3a0a8629f0cd31de4eb2d868b35bc427b286ac5b
tdkeating <tkeating26@yahoo.com>
2022-01-28 17:14:18 -0800

    added cumulative event table

--- analyses/capstone/km_curve.R
+++ analyses/capstone/km_curve.R
@@ -33,31 +33,47 @@ db_hhsaw <- DBI::dbConnect(odbc::odbc(),
 # Select table that contains time-to-homelessness variable
 tth_data<- setDT(DBI::dbGetQuery(conn = db_hhsaw, "SELECT * FROM [hudhears].[capstone_data_2]"))
 
+
+#-------------------------------------
 # Create "survival" object column in data
 tth_data<- tth_data %>% mutate(surv_object= Surv(time=tt_homeless, event=event))
 
 # Create survival curve by exit type
 fit_exit_type<- survfit(surv_object ~ exit_category, data=tth_data, conf.type="log-log")
+#-------------------------------------
 
+
+#-------------------------------------
+## Plotting
 # Create plot of Kaplan-Meier survival curve by exit type
 km_exit_type<- ggsurvplot(fit_exit_type, data=tth_data,
                             xlab="Time from Public Housing Exit (Days)",
                             ylab="Proportion Remaining Housed",
                             title="Kaplan-Meier Estimates of Time from Exit to Homelessness",
-                            break.time.by=500,
-                            #xlim=c(0,max(tth_data$tt_homeless)),
-                            ylim=c(0,1),
+                            break.time.by=50,
+                            xlim=c(0,max(tth_data$tt_homeless)),
+                            ylim=c(0.5,1),
                             conf.int=TRUE,
                             legend.title="Exit Type",
                             legend.labs= unique(tth_data$exit_category),
                             risk.table=TRUE, # risk table added
-                            risk.table.col="strata",
-                            risk.table.height=0.3,
+                            cumevents=TRUE, # cumulative events table added
+                            tables.col="strata",
+                            #tables.height=0.3,
                             censor.size=4,
                             gg_theme=theme_bw())
 # remove x-lab from risk table
 km_exit_type$table<- km_exit_type$table +
   labs(x="")
+km_exit_type$cumevents<- km_exit_type$cumevents + 
+  labs(x="")
 #km_exit_type
+#-------------------------------------
+
+
+#-------------------------------------
+## Other Descriptive Stats
+# Non-parametric KM-estimated survival at time= 365 days
+summary(fit_exit_type, times=365)
 
 

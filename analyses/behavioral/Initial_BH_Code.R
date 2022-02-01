b408d96728038ba4c458dcf440a8a8bdf0dc276f
Megan Suter <n-mesuter@kingcounty.gov>
2022-02-01 11:24:41 -0800

    Demogs by Medicaid Status

--- analyses/behavioral/Initial_BH_Code.R
+++ analyses/behavioral/Initial_BH_Code.R
@@ -7,4 +7,23 @@
 # 3) Use cross-walk tables to join tables of interest
 #   a) Study ID to KCID
 #   b) KCID to auth_id file (for crisis events)
+#   c) Also joint with Medicaid ID (If applicable)
 # 4) Demographic comparisons
+
+
+#Connect to HHSAW Server
+library(DBI)
+db_hhsaw <- DBI::dbConnect(odbc::odbc(),
+                           driver = "ODBC Driver 17 for SQL Server",
+                           server = "tcp:kcitazrhpasqlprp16.azds.kingcounty.gov,1433",
+                           database = "hhs_analytics_workspace",
+                           uid = keyring::key_list("hhsaw")[["username"]],
+                           pwd = keyring::key_get("hhsaw", keyring::key_list("hhsaw")[["username"]]),
+                           Encrypt = "yes",
+                           TrustServerCertificate = "yes",
+                           Authentication = "ActiveDirectoryPassword")
+
+#Read in tables of interest
+
+
+

48796b23a9a1c1cbe5ef83356b7176f3aced4429
Hantong Hu <42748248+hantongh@users.noreply.github.com>
2022-02-09 11:43:30 -0800

    Add files via upload

--- analyses/capstone/table1.R
+++ analyses/capstone/table1.R
@@ -26,7 +26,7 @@ message("Hint! There are different flavors of SQL. When googling for how to do s
             be sure to look for TSQL | T-SQL | Transact-SQL")
 
 # Create a connection to hhs_analytics_workspace on Azure server 16 ----
-cxn16 <- DBI::dbConnect(odbc::odbc(), 
+db_hhsaw <- DBI::dbConnect(odbc::odbc(), 
                         driver = "ODBC Driver 17 for SQL Server", 
                         server = "kcitazrhpasqlprp16.azds.kingcounty.gov", 
                         database = "hhs_analytics_workspace", 
@@ -36,16 +36,14 @@ cxn16 <- DBI::dbConnect(odbc::odbc(),
                         TrustServerCertificate = "yes", 
                         Authentication = "ActiveDirectoryPassword")
 
-df <- setDT(DBI::dbGetQuery(conn = cxn16, "SELECT * FROM [hudhears].[capstone_data_2]"))
-# df1 <- setDT(DBI::dbGetQuery(conn = cxn16, "SELECT * FROM [hudhears].[capstone_data_1]"))
-# head(df1)
+df <- setDT(DBI::dbGetQuery(conn = db_hhsaw, "SELECT * FROM [hudhears].[capstone_data_4]"))
 head(df)
 
 ### Descriptive stats
 ## Variables in the table:
-## exit_date?, gender_me, race_eth_me, age_at_exit, housing_time_at_exit, agency,
-## major_prog, prog_type, hh_size, hh_disability, single_caregiver, 
-## exit_category, tt_homeless, event
+## exit_date?, gender_me, race_eth_me, age_at_exit, new_housing_time, agency
+## new_prog_type, hh_size, hh_disability, single_caregiver, 
+## exit_category, tt_homeless, event, kc_opp_index_score
 
 library(table1)
 df.table <- df
@@ -60,44 +58,55 @@ df.table$event <- factor(df.table$event,
                          levels = c(0,1),
                          labels = c("Yes","No"))
 
-label(df.table$gender_me) <- "Sex"
+label(df.table$gender_me) <- "Gender"
 label(df.table$race_eth_me) <- "Race/Ethnicity"
 label(df.table$age_at_exit) <- "Age at exit (year)"
-label(df.table$housing_time_at_exit) <- "Time in housing at exit (year)"
+label(df.table$new_housing_time) <- "Time in housing at exit (year)"
 label(df.table$agency) <- "Agency"
+# label(df.table$new_prog_type) <- "Subsidy type"
 label(df.table$major_prog) <- "Subsidy type"
-label(df.table$prog_type) <- "Specific program type"
+# label(df.table$prog_type) <- "Specific program type"
 label(df.table$hh_size) <- "Household size"
 label(df.table$hh_disability) <- "Head of household disabled"
 label(df.table$single_caregiver) <- "Single caregiver in household"
 label(df.table$tt_homeless) <- "Time to homeless (day)"
 label(df.table$event) <- "Censored"
+label(df.table$kc_opp_index_score) <- "Opportunity index score"
 
 # Censored
-table1(~gender_me+race_eth_me+age_at_exit+housing_time_at_exit+agency
-       +major_prog+prog_type+hh_size+hh_disability+single_caregiver|exit_category, 
+table1(~gender_me+race_eth_me+age_at_exit+new_housing_time+agency+
+               major_prog+hh_size+hh_disability+single_caregiver+
+               kc_opp_index_score|exit_category, 
        data=(df.table %>% subset(event=="Yes")))
 
 # Non-censored
-table1(~gender_me+race_eth_me+age_at_exit+housing_time_at_exit+agency
-       +major_prog+prog_type+hh_size+hh_disability+single_caregiver|exit_category, 
+table1(~gender_me+race_eth_me+age_at_exit+new_housing_time+agency+
+         major_prog+hh_size+hh_disability+single_caregiver+
+         kc_opp_index_score|exit_category, 
        data=(df.table %>% subset(event=="No")))
 
 # Overall
-table1(~tt_homeless+event+gender_me+race_eth_me+age_at_exit+housing_time_at_exit+agency
-       +major_prog+prog_type+hh_size+hh_disability+single_caregiver|exit_category, 
+table1(~tt_homeless+event+gender_me+race_eth_me+age_at_exit+new_housing_time+
+         agency+major_prog+hh_size+hh_disability+single_caregiver+
+         kc_opp_index_score|exit_category, 
        data=df.table)
 
-df <- rename(df, housing_time=housing_time_at_exit)
+### Missingness pattern
 library(VIM)
-aggr_plot <- aggr(df[,c(6:15,17,21,22)],
+df.miss <- df %>% select(gender_me, race_eth_me, age_at_exit, new_housing_time, 
+                         agency, major_prog, hh_size, hh_disability, 
+                         single_caregiver, kc_opp_index_score, 
+                         exit_reason_clean, exit_category)
+df.miss <- rename(df.miss, opp_score=kc_opp_index_score)
+
+aggr_plot <- aggr(df.miss,
                   col=c("navyblue","red"),
-                  bars=F,prop=F,combined=T,
+                  bars=F,prop=F,combined=F,
                   numbers=T,
                   sortVars=F,
                   cex.lab=1.2,
                   cex.axis=0.54,
                   #labels=names(df[,c(6:15,17,21,22)]),
                   ylab=c("Histogram of missing data", "Pattern"))
-labs <- paste(names(df[,c(6:15,17,21,22)]))
-text(cex=1, x=-.25, y=-1.25, labs, xpd=TRUE, srt=45)
+# labs <- paste(names(df[,c(6:15,17,21,22)]))
+# text(cex=1, x=-.25, y=-1.25, labs, xpd=TRUE, srt=45)

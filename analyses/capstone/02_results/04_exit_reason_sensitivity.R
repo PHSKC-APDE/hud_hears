16005f824837b3a24e51e0ea17d5b7372cd1c53d
tdkeating <tkeating26@yahoo.com>
2022-02-16 20:30:51 -0800

    run analysis with multinom log reg w/o GEE
    also updated forestplots

--- analyses/capstone/02_results/04_exit_reason_sensitivity.R
+++ analyses/capstone/02_results/04_exit_reason_sensitivity.R
@@ -2,7 +2,7 @@
 ##
 ## Purpose of script: 
 ##    1) Create vector of exit reasons for sensitivity analysis 
-##    2) Create function to perform analysis
+##    2) Create functions to perform analysis (with and without GEE in multinomial log reg)
 ##    3) Create function to omit an exit reason and perform analysis
 ##    4) Apply fit_one_out() function over vector of exit reasons
 ##    5) Create Forest Plots
@@ -12,9 +12,8 @@
 ## Email: n-tkeating@kingcounty.gov
 ##
 ## Notes: In progress, still updating
-##          - filter for exit reasons that have certain count
-##          - problem with 2 exit reasons right now
-##          - maybe use ggforestplot or ggplot to create forest plots
+##          - Problem with 2 exit reasons ("Moved to Non-Subsidized Rental" and "Rent too high") while fitting multinomial log reg with GEE 
+##          - Therefore, all runs in this sensitivity analysis are performed used multinomial log reg W/O GEE
 ##   
 ##
 
@@ -25,7 +24,7 @@ options(dplyr.summarise.inform = FALSE)
 
 if (!require("pacman")) {install.packages("pacman")}
 pacman::p_load(tidyverse, odbc, glue, data.table, ggplot2, viridis, hrbrthemes,
-               knitr, kableExtra, rmarkdown, multgee, survival, forestplot)
+               knitr, kableExtra, rmarkdown, multgee, survival, forestplot, nnet)
 
 # Connect to HHSAW
 db_hhsaw <- DBI::dbConnect(odbc::odbc(),
@@ -38,8 +37,9 @@ db_hhsaw <- DBI::dbConnect(odbc::odbc(),
                            TrustServerCertificate = "yes",
                            Authentication = "ActiveDirectoryPassword")
 
-# Select table that contains study data
+# Select table that contains study data and re-level exit category with reference as "Neutral"
 study_data<- setDT(DBI::dbGetQuery(conn = db_hhsaw, "SELECT * FROM [hudhears].[capstone_data_3]"))
+study_data$exit_category<- relevel(factor(study_data$exit_category), ref="Neutral")
 
 #--------------------------------------------------
 # 1) Create list of exit reasons for sensitivity analysis
@@ -48,9 +48,11 @@ exit_reason_vector<- study_data$exit_reason_clean %>% unique()
 exit_reason_vector<- exit_reason_vector[is.na(exit_reason_vector)==FALSE] # omit NA exit_reason_clean from vector
 
 #--------------------------------------------------
-# 2) Create function to perform analysis on study data
+# 2) Create 2 functions to perform analysis on study data
+  # i) with GEE in multinomial log reg
+  # ii) without GEE in multinomial log reg
 
-# function runs analysis on dataset and returns fitted Cox PH model
+# function runs analysis (with GEE in multinomial log reg) on dataset and returns fitted Cox PH model
 run_analysis<- function(data){
   
   # fit multinomial log reg model with GEE and calculate generalized propensity scores
@@ -62,7 +64,7 @@ run_analysis<- function(data){
                       id = hh_id_kc_pha,
                       LORstr = "independence")
   ps <- as.data.frame(fitted(ps_mod))
-  colnames(ps) <- c("Neutral", "Negative", "Positive")
+  colnames(ps) <- levels(data$exit_category) 
   
   # Then, calculate inverse weights and assign to each obs.
   data$IPTW <- ifelse(data$exit_category == "Negative", 1/ps$Negative,
@@ -70,11 +72,35 @@ run_analysis<- function(data){
                                  1/ps$Positive))
   
   # Finally, fit weighted Cox PH (cluster on hh_id_kc_pha)
-  data$exit_category <- relevel(factor(data$exit_category), ref = "Neutral")
   tth_mod <- coxph(formula = Surv(tt_homeless, event) ~ exit_category,
                    data = data,
                    weights = IPTW,
                    cluster = hh_id_kc_pha)
+  return(tth_mod)
+}
+
+# function runs analysis (without GEE in multinomial log reg) on dataset and returns fitted Cox PH model
+run_analysis_no_GEE<- function(data){
+  
+  # fit multinomial log reg model without GEE and calculate generalized propensity scores
+  ps_mod<- multinom(formula= exit_category ~ age_at_exit + gender_me +
+                      race_eth_me + agency + single_caregiver + hh_size +
+                      hh_disability + housing_time_at_exit + major_prog +
+                      kc_opp_index_score,
+                    data= data,
+                    trace=FALSE) 
+  ps<- as.data.frame(fitted(ps_mod))
+  
+  # Then, calculate inverse weights and assign to each obs.
+  data$IPTW<- ifelse(data$exit_category== "Negative", 1/ps$Negative,
+                         ifelse(data$exit_category== "Neutral", 1/ps$Neutral,
+                                1/ps$Positive))
+  
+  # Finally, fit weighted Cox PH (cluster on hh_id_kc_pha)
+  tth_mod<- coxph(formula = Surv(tt_homeless, event) ~ exit_category,
+                  data = data,
+                  weights = IPTW,
+                  cluster = hh_id_kc_pha)
   return(tth_mod)
 }
 
@@ -84,21 +110,21 @@ run_analysis<- function(data){
 # function that takes an exit reason, and the full dataset
   # omits rows with that reason from full dataset,
   # performs analysis 
-  # and returns number omitted, HR estimates, and CI's for pos and neg
+  # and returns number omitted, HR estimates, and CI's for pos vs. neut and neg vs. neut
 fit_one_out<- function(reason_omitted, data){
+  
+  # print current reason
+  print(reason_omitted)
   
   # omit rows with that specified exit reason
   new_data<- data %>% filter(exit_reason_clean != reason_omitted) 
   
-  # perform analysis and return the fitted Cox PH model
-  tth_mod<- run_analysis(new_data)
-  
-  #remove later
-  print(reason_omitted)
+  # run analysis w/o GEE in multinomial log reg, return fitted Cox PH model
+  tth_mod<- run_analysis_no_GEE(new_data)
   
   # return reason omitted, reason category, number omitted, HR estimates and CI's for pos and neg
   return(list(exit_reason_omitted= reason_omitted,
-              exit_category= data %>% filter(exit_reason_clean==reason_omitted) %>% pull(exit_category) %>% unique(), 
+              exit_category= data %>% filter(exit_reason_clean==reason_omitted) %>% pull(exit_category) %>% unique() %>% as.character(), 
               number_omitted= data %>% filter(exit_reason_clean==reason_omitted) %>% nrow(),
               HR_pos= (tth_mod %>% coef() %>% exp())[["exit_categoryPositive"]],
               HR_pos_lower= (tth_mod %>% confint() %>% exp())["exit_categoryPositive", 1],
@@ -114,24 +140,17 @@ fit_one_out<- function(reason_omitted, data){
 # 4) Apply fit_one_out function over vector of exit reasons to study data
 
 # HR estimates and CI's from loo sensitivity analysis
+loo_output<- as.tibble(t(sapply(exit_reason_vector, FUN=fit_one_out, data=study_data)))
 
-# # REMOVE LATER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# run_analysis(study_data %>% filter(exit_reason_clean!= "Rent too high")) # system is computationally singular
-# run_analysis(study_data %>% filter(exit_reason_clean!= "Moved to Non-Subsidized Rental")) # Robust covariance matrix is not positive definite
-# # REMOVE LATER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-# perform without "Moved to Non-Subsidized Rental" and "Rent too high" FOR NOW
-loo_output<- as_tibble(t(sapply(exit_reason_vector[exit_reason_vector!="Moved to Non-Subsidized Rental" & 
-                                                     exit_reason_vector!="Rent too high"], FUN=fit_one_out, data=study_data)))
-
-# format loo_output
-loo_output$exit_category<-  relevel(factor(loo_output$exit_category), ref = "Neutral") 
-loo_output<- loo_output %>% arrange(exit_category, desc(as.numeric(number_omitted))) # arrange in descending order by number omitted within exit_category
+# format loo_output (make columns character/numeric vectors instead of lists)
+# arrange in descending order by number omitted within exit_category
 loo_output[,1:3]<- sapply(loo_output[,1:3], MARGIN=2, as.character)
 loo_output[,4:ncol(loo_output)]<- sapply(loo_output[,4:ncol(loo_output)], MARGIN=2, as.numeric)
+loo_output$exit_category<-  relevel(factor(loo_output$exit_category), ref = "Neutral") 
+loo_output<- loo_output %>% arrange(exit_category, desc(as.numeric(number_omitted)))
 
-# Full Data HR estimates and CI's
-full_data_mod<- run_analysis(study_data)
+# Full Data HR estimates and CI's (analysis using multinomial log reg w/o GEE)
+full_data_mod<- run_analysis_no_GEE(study_data)
 full_data_output<- 
   tibble(exit_reason_omitted= "Full Data",
          exit_category= "Full Data",
@@ -148,59 +167,124 @@ full_data_output<-
 # 5) Create Forest Plots
 ## using forestplot::forestplot()
 
-# set count threshold to plot
+# i) set count threshold
+# ii) format dataframe for forest plot (header and summary rows)
+# iii) plots of HR pos vs neutral
+# iv) plots of HR neg vs neutral
+
+#---
+## i) set count threshold to plot
 count_threshold<- 100
 
-# create forest plot data frame with header and summary row
-header<- tibble(exit_reason_omitted= "Exit Reason Omitted",
-                number_omitted= "Number Omitted",
-                exit_category= "Exit Category",
-                summary=TRUE)
-empty_row<- tibble(exit_reason_omitted=NA)
-
-# only take rows of exit reasons with count >= threshold
-forest_plot_data<- bind_rows(header,
-                             loo_output %>% filter(as.numeric(number_omitted)>=count_threshold),
-                             empty_row,
-                             full_data_output)
 #---
-#png(file="HR_pos_LOO.png", width=1400, height=1200)
-# forest plot for HR positive vs neutral
+## ii) format data frame for forest plot
+forest_plot_data<- bind_rows(
+  # header row
+  tibble(exit_reason_omitted= "Exit Reason Omitted", 
+         number_omitted= "Number Omitted",
+         exit_category= "Exit Category",
+         summary=TRUE),
+  # LOO data with only rows of exit reasons with count >= threshold
+  loo_output %>% filter(as.numeric(number_omitted)>=count_threshold), 
+  # empty row
+  tibble(exit_reason_omitted=NA), 
+  # results from full model
+  full_data_output)
+
+#---
+## iii) plot HR Positive vs Neutral
+
+# forest plot for HR positive vs neutral (FULL PLOT)
+png(file="LOO_HR_pos_no_GEE.png", width=1400, height=1200)
 forest_plot_data %>%
   forestplot::forestplot(labeltext= c(exit_reason_omitted, number_omitted, exit_category),
              txt_gp= fpTxtGp(label=gpar(cex=1), # label text size might be too large to plot (adjust here to 0.25)
-                             xlab=gpar(cex=1.25),
-                             title=gpar(cex=1.5),
+                             xlab=gpar(cex=1.5),
+                             title=gpar(cex=2),
                              ticks=gpar(cex=1)),
              is.summary= summary,
              mean= HR_pos,
              lower= HR_pos_lower,
              upper= HR_pos_upper,
              title= "Positive vs. Neutral Exit",
-             xlab="Hazard Ratio",
+             xlab="Hazard Ratio of Experiencing Homelessness",
              zero= forest_plot_data %>% filter(exit_reason_omitted=="Full Data") %>% pull(HR_pos),
              grid= c(forest_plot_data %>% filter(exit_reason_omitted=="Full Data") %>% pull(HR_pos_lower),
-                     forest_plot_data %>% filter(exit_reason_omitted=="Full Data") %>% pull(HR_pos_upper))
+                     forest_plot_data %>% filter(exit_reason_omitted=="Full Data") %>% pull(HR_pos_upper)),
+             hrzl_lines=list("2" = gpar(lwd=1, columns=c(1:3)), 
+                             "27" = gpar(lwd= 442, lineend="butt", columns=c(1:3), col="#99999922"))
              )
-#dev.off()
+dev.off()
 
-#png(file="HR_neg_LOO.png", width=1400, height=1200)
-# forest plot for HR negative vs neutral
+# forest plot for HR positive vs neutral (REMOVING NEGATIVE REASONS)
+png(file="LOO_HR_pos_no_GEE_no_neg.png", width=1400, height=1200)
+forest_plot_data %>% filter((exit_category %in% c("Neutral", "Positive", "Exit Category", "Full Data")) |
+                              is.na(exit_category)) %>%
+  forestplot::forestplot(labeltext= c(exit_reason_omitted, number_omitted, exit_category),
+                         txt_gp= fpTxtGp(label=gpar(cex=1), # label text size might be too large to plot (adjust here to 0.25)
+                                         xlab=gpar(cex=1.5),
+                                         title=gpar(cex=2),
+                                         ticks=gpar(cex=1)),
+                         is.summary= summary,
+                         mean= HR_pos,
+                         lower= HR_pos_lower,
+                         upper= HR_pos_upper,
+                         title= "Positive vs. Neutral Exit",
+                         xlab="Hazard Ratio of Experiencing Homelessness",
+                         zero= forest_plot_data %>% filter(exit_reason_omitted=="Full Data") %>% pull(HR_pos),
+                         grid= c(forest_plot_data %>% filter(exit_reason_omitted=="Full Data") %>% pull(HR_pos_lower),
+                                 forest_plot_data %>% filter(exit_reason_omitted=="Full Data") %>% pull(HR_pos_upper)),
+                         hrzl_lines=list("2" = gpar(lwd=1, columns=c(1:3)), 
+                                         "21" = gpar(lwd=240, lineend="butt", columns=c(1:3), col="#99999922"))
+  )
+dev.off()
+
+#---
+## iv) plot HR Negative vs Neutral
+
+# forest plot for HR negative vs neutral (FULL PLOT)
+png(file="LOO_HR_neg_no_GEE.png", width=1400, height=1200)
 forest_plot_data %>% 
   forestplot::forestplot(labeltext= c(exit_reason_omitted, number_omitted, exit_category),
              txt_gp= fpTxtGp(label=gpar(cex=1),  # label text size might be too large to plot (adjust here to 0.25)
-                             xlab=gpar(cex=1.25),
-                             title=gpar(cex=1.5),
+                             xlab=gpar(cex=1.5),
+                             title=gpar(cex=2),
                              ticks=gpar(cex=1)),
              is.summary= summary,
              mean= HR_neg,
              lower= HR_neg_lower,
              upper= HR_neg_upper,
              title= "Negative vs. Neutral Exit",
-             xlab="Hazard Ratio",
+             xlab="Hazard Ratio of Experiencing Homelessness",
              zero= forest_plot_data %>% filter(exit_reason_omitted=="Full Data") %>% pull(HR_neg),
              grid= c(forest_plot_data %>% filter(exit_reason_omitted=="Full Data") %>% pull(HR_neg_lower),
-                     forest_plot_data %>% filter(exit_reason_omitted=="Full Data") %>% pull(HR_neg_upper))
+                     forest_plot_data %>% filter(exit_reason_omitted=="Full Data") %>% pull(HR_neg_upper)),
+             hrzl_lines=list("2" = gpar(lwd=1,columns=c(1:3)), 
+                             "27" = gpar(lwd= 442, lineend="butt", columns=c(1:3), col="#99999922"))
              )
-#dev.off()
+dev.off()
+
+# forest plot for HR negative vs neutral (REMOVING POSITIVE REASONS)
+png(file="LOO_HR_neg_no_GEE_no_pos.png", width=1400, height=1200)
+forest_plot_data %>% filter((exit_category %in% c("Neutral", "Negative", "Exit Category", "Full Data")) |
+                              is.na(exit_category)) %>%
+  forestplot::forestplot(labeltext= c(exit_reason_omitted, number_omitted, exit_category),
+                         txt_gp= fpTxtGp(label=gpar(cex=1),  # label text size might be too large to plot (adjust here to 0.25)
+                                         xlab=gpar(cex=1.5),
+                                         title=gpar(cex=2),
+                                         ticks=gpar(cex=1)),
+                         is.summary= summary,
+                         mean= HR_neg,
+                         lower= HR_neg_lower,
+                         upper= HR_neg_upper,
+                         title= "Negative vs. Neutral Exit",
+                         xlab="Hazard Ratio of Experiencing Homelessness",
+                         zero= forest_plot_data %>% filter(exit_reason_omitted=="Full Data") %>% pull(HR_neg),
+                         grid= c(forest_plot_data %>% filter(exit_reason_omitted=="Full Data") %>% pull(HR_neg_lower),
+                                 forest_plot_data %>% filter(exit_reason_omitted=="Full Data") %>% pull(HR_neg_upper)),
+                         hrzl_lines=list("2" = gpar(lwd=1, columns=c(1:3)), 
+                                         "27" = gpar(lwd= 520, lineend="butt", columns=c(1:3), col="#99999922"))
+  )
+dev.off()
+
 

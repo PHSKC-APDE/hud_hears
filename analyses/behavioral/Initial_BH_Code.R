1f71f0d96bd9ccf199f774abb13aec383425ab58
Megan Suter <n-mesuter@kingcounty.gov>
2022-03-08 18:05:30 -0800

    Analysis updates

--- analyses/behavioral/Initial_BH_Code.R
+++ analyses/behavioral/Initial_BH_Code.R
@@ -27,36 +27,96 @@ db_hhsaw <- DBI::dbConnect(odbc::odbc(),
 #Load necessary packages
 pacman::p_load(data.table, DBI, glue, dplyr, tidyr, lubridate)
 
-#Load data table 
-#Load some data in to test
-#Load some data in to test
-##Relevant tables to work with
-control_match_covariate <- setDT(DBI::dbGetQuery(conn = db_hhsaw, "SELECT * FROM [hudhears].[control_match_covariate]"))
+# #Load data table 
+# #Load some data in to test
+# #Load some data in to test
+# ##Relevant tables to work with
+# control_match_covariate <- setDT(DBI::dbGetQuery(conn = db_hhsaw, "SELECT * FROM [hudhears].[control_match_covariate]"))
+# 
+# control_match_long <- setDT(DBI::dbGetQuery(conn = db_hhsaw, "SELECT * FROM [hudhears].[control_match_long]"))
+# 
+# control_match <- setDT(DBI::dbGetQuery(conn = db_hhsaw, "SELECT * FROM [hudhears].[control_match]"))
+# 
+# ##Left join this table with Medicaid ID table
+# control_match_id_mcaid <- dbGetQuery(db_hhsaw,
+#                                      "SELECT a.*, b.id_mcaid
+#                                      FROM
+# (SELECT * FROM hudhears.control_match_long) a
+# LEFT JOIN
+# (SELECT DISTINCT id_hudhears, id_mcaid
+# FROM claims.hudhears_id_xwalk WHERE id_mcaid is not null) b
+# ON a.id_hudhears = b.id_hudhears") %>%
+#   mutate(exit_1yr_prior = exit_date - years(1) + days(1),
+#          exit_1yr_after = exit_date + years(1) - days(1))
+# 
+# 
+# ## Observations off by 300
+# # Do antijoin to see why this is happening
+# 
+# mis_match<- anti_join(control_match_id_mcaid, control_match_covariate, by=c("id_hudhears"))
+# mis_match2<- as.data.frame(mis_match)
+# 
+# 
+# 
+# 
+# control_match_covariate %>% select(id_hudhears, starts_with("full_")) %>% head() %>% mutate(chk = full_cov_7_prior * full_cov_7_after)
 
-control_match_long <- setDT(DBI::dbGetQuery(conn = db_hhsaw, "SELECT * FROM [hudhears].[control_match_long]"))
 
-control_match <- setDT(DBI::dbGetQuery(conn = db_hhsaw, "SELECT * FROM [hudhears].[control_match]"))
+##Join covariate table BH events-includes, crisis, ITAs, and outpatient BH events (all non-Medicaid)
+covariate <- dbGetQuery(db_hhsaw, "SELECT * FROM hudhears.control_match_covariate WHERE id_type = 'id_exit'")
 
-##Left join this table with Medicaid ID table
-control_match_id_mcaid <- dbGetQuery(db_hhsaw,
-                                     "SELECT a.*, b.id_mcaid
-                                     FROM
-(SELECT * FROM hudhears.control_match_long) a
+exits_bh <- dbGetQuery(db_hhsaw,
+                       "SELECT a.id_hudhears, a.gender_me, a.exit_date, DATEADD(year, 1, a.exit_date) AS yr_later, b.crisis_date AS event_date, b.source
+                       FROM
+                       (SELECT id_hudhears, exit_date, gender_me 
+                         FROM hudhears.control_match_covariate 
+                         WHERE id_type = 'id_exit' AND exit_death = 0 AND exit_date <= '2018-12-31') a
+                       LEFT JOIN
+                       (SELECT DISTINCT id_hudhears, program, description, crisis_date, 'crisis' AS source
+                         FROM hudhears.bh_crisis_events) b
+                       ON a.id_hudhears = b.id_hudhears
+                       AND b.crisis_date >= a.exit_date AND b.crisis_date <= DATEADD(year, 1, a.exit_date)
+                       --WHERE b.crisis_date IS NOT NULL
+                       
+                       UNION
+                       
+                       SELECT x.id_hudhears, x.gender_me, x.exit_date, DATEADD(year, 1, x.exit_date) AS yr_later, y.call_date AS event_date, y.source
+                       FROM
+                       (SELECT id_hudhears, exit_date, gender_me 
+                         FROM hudhears.control_match_covariate 
+                         WHERE id_type = 'id_exit' AND exit_death = 0 AND exit_date <= '2018-12-31') x
+                       LEFT JOIN
+                       (SELECT DISTINCT id_hudhears, call_date, 'ita' AS source FROM hudhears.bh_ita_events) y
+                       ON x.id_hudhears = y.id_hudhears
+                       AND y.call_date >= x.exit_date AND y.call_date <= DATEADD(year, 1, x.exit_date)
+                       --WHERE y.call_date IS NOT NULL)
+
+                       UNION
+SELECT z.id_hudhears, z.gender_me, z.exit_date, DATEADD(year, -1, z.exit_date) AS yr_before, d.event_date AS event_date, d.source
+FROM
+(SELECT id_hudhears, exit_date, gender_me 
+  FROM hudhears.control_match_covariate 
+  WHERE id_type = 'id_exit' AND exit_death = 0) z
 LEFT JOIN
-(SELECT DISTINCT id_hudhears, id_mcaid
-FROM claims.hudhears_id_xwalk WHERE id_mcaid is not null) b
-ON a.id_hudhears = b.id_hudhears") %>%
-  mutate(exit_1yr_prior = exit_date - years(1) + days(1),
-         exit_1yr_after = exit_date + years(1) - days(1))
-
-
-## Observations off by 300
-# Do antijoin to see why this is happening
-
-mis_match<- anti_join(control_match_id_mcaid, control_match_covariate, by=c("id_hudhears"))
-mis_match2<- as.data.frame(mis_match)
+(SELECT DISTINCT id_hudhears, event_date, 'outpt' AS source FROM hudhears.bh_outpatient_events) d
+ON z.id_hudhears = d.id_hudhears
+AND d.event_date <= z.exit_date AND d.event_date >= DATEADD(year, -1, z.exit_date)
+WHERE d.event_date IS NOT NULL
+                       ")
+ #Get exit type variable from relevant table
+#Note: only getting that one variable
+pha_stage <- dbGetQuery(db_hhsaw, "SELECT * FROM pha.stage_pha_exit_timevar WHERE act_date IS NOT NULL")
 
 
 
 
-control_match_covariate %>% select(id_hudhears, starts_with("full_")) %>% head() %>% mutate(chk = full_cov_7_prior * full_cov_7_after)
+exits_bh_type <- left_join(exits_bh, distinct(pha_stage, id_hudhears, act_date, exit_category), by=c("id_hudhears", "exit_date"="act_date"))
+
+
+
+
+exits_bh_sum <- exits_bh %>% group_by(id_hudhears, exit_date, exit_category) %>% summarise(event_count = n_distinct(event_date, na.rm = T)) %>% ungroup() 
+exits_bh %>% filter(source %in% c("crisis", "ita"))%>%summarize(events=n_distinct (id_hudhears))
+
+
+
--- analyses/behavioral/STROBE_dia.R
+++ analyses/behavioral/STROBE_dia.R
@@ -88,28 +88,28 @@ exit_timevar <- dbGetQuery(db_hhsaw,
 # #Excluding 19736-19329=407 exits that were duplicates
 
 
-##Write over this with correct code (or just run this part)
-# Set up list of IDs, exits, and dates
-exits <- exit_timevar %>% 
-  filter(true_exit == 1 & 
-           ((agency == "SHA" & act_date >= "2012-01-01" & act_date <= "2018-12-31") | 
-              (agency == "KCHA" & act_date >= "2016-01-01" & act_date <= "2018-12-31"))) %>%
-  distinct(id_kc_pha, act_date) %>%
-  arrange(id_kc_pha, act_date) %>%
-# Take the most recent exit for the 400 people with multiple exits
-  group_by(id_kc_pha) %>%
-  mutate(exit_cnt = n(), exit_order = row_number()) %>% ungroup() %>%
-  filter(exit_cnt == exit_order)
-
-# Note: Control matching was not rerun due to long run time. 
-#Read in pre-generated table of exits and matched controls
-control_match_long <- setDT(DBI::dbGetQuery(conn = db_hhsaw, "SELECT * FROM [hudhears].[control_match_long]"))
-#Tabulate Ids to check number of matched controls
-table(control_match_long$id_type)
-# id_control    id_exit 
-# 76936      19234 
-
-#19234 vs. 19329
+# ##Write over this with correct code (or just run this part)
+# # Set up list of IDs, exits, and dates
+# exits <- exit_timevar %>% 
+#   filter(true_exit == 1 & 
+#            ((agency == "SHA" & act_date >= "2012-01-01" & act_date <= "2018-12-31") | 
+#               (agency == "KCHA" & act_date >= "2016-01-01" & act_date <= "2018-12-31"))) %>%
+#   distinct(id_kc_pha, act_date) %>%
+#   arrange(id_kc_pha, act_date) %>%
+# # Take the most recent exit for the 400 people with multiple exits
+#   group_by(id_kc_pha) %>%
+#   mutate(exit_cnt = n(), exit_order = row_number()) %>% ungroup() %>%
+#   filter(exit_cnt == exit_order)
+# 
+# # Note: Control matching was not rerun due to long run time. 
+# #Read in pre-generated table of exits and matched controls
+# control_match_long <- setDT(DBI::dbGetQuery(conn = db_hhsaw, "SELECT * FROM [hudhears].[control_match_long]"))
+# #Tabulate Ids to check number of matched controls
+# table(control_match_long$id_type)
+# # id_control    id_exit 
+# # 76936      19234 
+# 
+# #19234 vs. 19329
 
 #Read in table with covariates
 control_match_covariate <- setDT(DBI::dbGetQuery(conn = db_hhsaw, "SELECT * FROM [hudhears].[control_match_covariate]"))
@@ -135,56 +135,58 @@ ON a.id_hudhears = b.id_hudhears") %>%
 ## Observations off by 300
 # Do antijoin to see why this is happening
 
-mis_match<- anti_join(control_match_id_mcaid, control_match_covariate, by=c("id_hudhears"))
-mis_match2<- as.data.frame(mis_match)
+# mis_match<- anti_join(control_match_id_mcaid, control_match_covariate, by=c("id_hudhears"))
+# mis_match2<- as.data.frame(mis_match)
 
 #Several Medicaid IDs match to a single HUD HEARS ID. Will take first record
 
 
-control_match_covariate %>% select(id_hudhears, starts_with("full_")) %>% head() %>% mutate(chk = full_cov_7_prior * full_cov_7_after)
+# control_match_covariate %>% select(id_hudhears, starts_with("full_")) %>% head() %>% mutate(chk = full_cov_7_prior * full_cov_7_after)
 
 ##Join covariate table to BH crisis events, ITAs, and outpatient events (all non-Medicaid)--then union these 3 new tables
 covariate <- dbGetQuery(db_hhsaw, "SELECT * FROM hudhears.control_match_covariate WHERE id_type = 'id_exit'")
-
 exits_bh <- dbGetQuery(db_hhsaw,
                        "SELECT a.id_hudhears, a.gender_me, a.exit_date, NULL as yr_before, DATEADD(year, 1, a.exit_date) AS yr_later, b.crisis_date AS event_date, b.source
-                       FROM
-                       (SELECT id_hudhears, exit_date, gender_me 
-                         FROM hudhears.control_match_covariate 
-                         WHERE id_type = 'id_exit' AND exit_death = 0 AND exit_date <= '2018-12-31') a
-                       LEFT JOIN
-                       (SELECT DISTINCT id_hudhears, program, description, crisis_date, 'crisis' AS source
-                         FROM hudhears.bh_crisis_events) b
-                       ON a.id_hudhears = b.id_hudhears
-                       AND b.crisis_date >= a.exit_date AND b.crisis_date <= DATEADD(year, 1, a.exit_date)
-                       WHERE b.crisis_date IS NOT NULL)
-                       
-                       UNION
-                       
-                       SELECT x.id_hudhears, x.gender_me, x.exit_date, NULL as yr_before, DATEADD(year, 1, x.exit_date) AS yr_later, y.call_date AS event_date, y.source
-                       FROM
-                       (SELECT id_hudhears, exit_date, gender_me 
-                         FROM hudhears.control_match_covariate 
-                         WHERE id_type = 'id_exit' AND exit_death = 0 AND exit_date <= '2018-12-31') x
-                       LEFT JOIN
-                       (SELECT DISTINCT id_hudhears, call_date, 'ita' AS source FROM hudhears.bh_ita_events) y
-                       ON x.id_hudhears = y.id_hudhears
-                       AND y.call_date >= x.exit_date AND y.call_date <= DATEADD(year, 1, x.exit_date)
-                       WHERE y.call_date IS NOT NULL)
+FROM
+(SELECT id_hudhears, exit_date, gender_me 
+  FROM hudhears.control_match_covariate 
+  WHERE id_type = 'id_exit' AND exit_death = 0) a
+LEFT JOIN
+(SELECT DISTINCT id_hudhears, program, description, crisis_date, 'crisis' AS source
+  FROM hudhears.bh_crisis_events) b
+ON a.id_hudhears = b.id_hudhears
+AND b.crisis_date >= a.exit_date AND b.crisis_date <= DATEADD(year, 1, a.exit_date)
+WHERE b.crisis_date IS NOT NULL
 
-                       UNION
-                       
-                      SELECT z.id_hudhears, z.gender_me, z.exit_date, DATEADD(year, -1, z.exit_date) AS yr_before, NULL as yr_after, d.event_date AS event_date, d.source
-                      FROM
-                      (SELECT id_hudhears, exit_date, gender_me 
-                        FROM hudhears.control_match_covariate 
-                        WHERE id_type = 'id_exit' AND exit_death = 0) z
-                      LEFT JOIN
-                      (SELECT DISTINCT id_hudhears, event_date, 'outpt' AS source FROM hudhears.bh_outpatient_events) d
-                        ON z.id_hudhears = d.id_hudhears
-                        AND d.event_date <= z.exit_date AND d.event_date >= DATEADD(year, -1, z.exit_date)
-                        WHERE d.event_date IS NOT NULL
-                       ")
+UNION
+
+SELECT x.id_hudhears, x.gender_me, x.exit_date, NULL as yr_before, DATEADD(year, 1, x.exit_date) AS yr_later, y.call_date AS event_date, y.source
+FROM
+(SELECT id_hudhears, exit_date, gender_me 
+  FROM hudhears.control_match_covariate 
+  WHERE id_type = 'id_exit' AND exit_death = 0)  x
+LEFT JOIN
+(SELECT DISTINCT id_hudhears, call_date, 'ita' AS source FROM hudhears.bh_ita_events) y
+ON x.id_hudhears = y.id_hudhears
+AND y.call_date >= x.exit_date AND y.call_date <= DATEADD(year, 1, x.exit_date)
+WHERE y.call_date IS NOT NULL
+UNION
+
+SELECT z.id_hudhears, z.gender_me, z.exit_date, DATEADD(year, -1, z.exit_date) AS yr_before, NULL as yr_after, d.event_date AS event_date, d.source
+FROM
+(SELECT id_hudhears, exit_date, gender_me 
+  FROM hudhears.control_match_covariate 
+  WHERE id_type = 'id_exit' AND exit_death = 0) z
+LEFT JOIN
+(SELECT DISTINCT id_hudhears, event_date, 'outpt' AS source FROM hudhears.bh_outpatient_events) d
+ON z.id_hudhears = d.id_hudhears
+AND d.event_date <= z.exit_date AND d.event_date >= DATEADD(year, -1, z.exit_date)
+WHERE d.event_date IS NOT NULL
+")
+
+
+
+
 #Get exit type variable from relevant table
 #Note: only getting that one variable
 
@@ -200,7 +202,7 @@ exits_bh %>% distinct() %>% count(source)
 #Number of IDs with any event
 exits_bh_type %>% filter(source %in% c("crisis", "ita"))%>%summarize(events=n_distinct (id_hudhears))
 #Count of events, grouped by exit type and ID
-exits_bh_type %>% filter(source %in% c("crisis", "ita"))%>% group_by(exit_category) %>% summarize(events=n_distinct (id_hudhears))
+exits_bh_type %>% filter(source %in% c("crisis", "ita"))%>% group_by(exit_category) %>% summarize(events=n_distinct (id_hudhears))%>% ungroup()
 # exit_category events
 # 1 Negative         212
 # 2 Neutral          241
@@ -216,8 +218,8 @@ exits_bh_type %>% group_by(exit_category) %>% summarize(n_distinct (id_hudhears)
 # 3 Positive                           3058
 
 
-exits_bh_type %>% group_by(exit_category) %>% mutate(totals=n_distinct (id_hudhears)) %>% filter(source %in% c("crisis", "ita"))%>% mutate(events=n_distinct (id_hudhears))%>% mutate(prop=events/totals)%>% count(prop)
-
+##Tabulate proportions
+# exits_bh_type %>% group_by(exit_category) %>% mutate(totals=n_distinct (id_hudhears)) %>% filter(source %in% c("crisis", "ita"))%>% mutate(events=n_distinct (id_hudhears))%>% mutate(prop=events/totals)%>% count(prop)
 
 # #Total numbers of events
 # exit_category    prop     n
@@ -226,8 +228,42 @@ exits_bh_type %>% group_by(exit_category) %>% mutate(totals=n_distinct (id_hudhe
 # 2 Neutral       0.0292    638
 # 3 Positive      0.00850    97
 
+#Make a data frame with number of events by person
+
+bh_count <- exits_bh_type %>% group_by(id_hudhears, exit_date) %>% filter(source %in% c("outpt")) %>% count()
+# summary(bh_count$n)
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 1.0     7.0    17.0    33.8    36.0   424.0 
+
+# hist(bh_count$n)
+
+#Make indicator for people with 2+ visits
+bh_count$reg_care <-ifelse(bh_count$n > 1, 1, 0)
+
+#Join this with other data frame
+bh_routine <- left_join(exits_bh_type, distinct(bh_count, id_hudhears, exit_date, n, reg_care), by=c("id_hudhears", "exit_date")) 
+
+#Summarize for all data
+bh_routine %>% group_by(reg_care) %>% summarize(n_distinct(id_hudhears))
+#Note that 174 were in the larger dataset who were duplicated IDs! (n=174)
+
+#Make recoded version of variable (change NA to 0, by ID)
+bh_routine$recode_reg_care <- replace(bh_routine$reg_care, is.na(bh_routine$reg_care),0)
 
 
+#Look at exit category by receiving regular care
+bh_routine %>% group_by(exit_category, recode_reg_care) %>% summarize(n_distinct (id_hudhears))
+
+
+
+
+
+
+
+
+#################################################################################
+
+#Attempts at flow chart
 library(DiagrammeR)
 grViz("digraph flowchart {
       # node definitions with substituted label text

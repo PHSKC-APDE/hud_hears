6087762282f3b6fad664ae4d2ac093e1ca304067
Danny Colombara <48296707+dcolombara@users.noreply.github.com>
2022-10-27 09:51:58 -0700

    define auth in 00_run_all
      -- needed to include definition of auth
         for each specific code file to be run

--- analyses/wages/final_report/00_run_all.R
+++ analyses/wages/final_report/00_run_all.R
@@ -10,21 +10,21 @@ pacman::p_load(httr)
 # 01_prep ----
   eval(parse(text = httr::content(httr::GET( 
     url = "https://raw.githubusercontent.com/PHSKC-APDE/hud_hears/main/analyses/wages/final_report/01_data_prep.R", 
-    httr::authenticate(auth, "")), "text"))) 
+    httr::authenticate(Sys.getenv("GITHUB_TOKEN"), "")), "text"))) 
 
 # 02_graph_raw ----
   eval(parse(text = httr::content(httr::GET( 
     url = "https://raw.githubusercontent.com/PHSKC-APDE/hud_hears/main/analyses/wages/final_report/02_graph_raw_wages.R", 
-    httr::authenticate(auth, "")), "text"))) 
+    httr::authenticate(Sys.getenv("GITHUB_TOKEN"), "")), "text"))) 
 
 # 03_descriptives ----
   eval(parse(text = httr::content(httr::GET( 
     url = "https://raw.githubusercontent.com/PHSKC-APDE/hud_hears/main/analyses/wages/final_report/03_tables_descriptive.R", 
-    httr::authenticate(auth, "")), "text"))) 
+    httr::authenticate(Sys.getenv("GITHUB_TOKEN"), "")), "text"))) 
 
 # 04_regression ----
   eval(parse(text = httr::content(httr::GET( 
     url = "https://raw.githubusercontent.com/PHSKC-APDE/hud_hears/main/analyses/wages/final_report/04_regression.R", 
-    httr::authenticate(auth, "")), "text"))) 
+    httr::authenticate(Sys.getenv("GITHUB_TOKEN"), "")), "text"))) 
 
 # The end ----
